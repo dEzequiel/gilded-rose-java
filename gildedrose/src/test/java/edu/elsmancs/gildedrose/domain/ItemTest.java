579e21658b57ccbf953c431311680cdc74250b92
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-02 23:51:23 +0100

    test(createItem): add item to verify Item object creation

--- gildedrose/src/test/java/edu/elsmancs/gildedrose/domain/ItemTest.java
+++ gildedrose/src/test/java/edu/elsmancs/gildedrose/domain/ItemTest.java
@@ -2,21 +2,15 @@ package edu.elsmancs.gildedrose.domain;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class ItemTest extends StockItem{
-
-    ItemTest(String name, int sellIn, int quality) {
-        super(name, sellIn, quality);
-        //TODO Auto-generated constructor stub
-    }
+public class ItemTest {
 
     @Test
     public void createItem() {
 
-        StockItem item = new StockItem("+5 Dexterity Vest", 10, 20);
+        Item item = new Item("+5 Dexterity Vest", 10, 20);
         assertEquals("+5 Dexterity Vest", item.name);
-        assertEquals(10, item.getSellIn(), 0);
-        assertEquals(20, item.getQuality(), 0);
-        System.out.println(item);    
+        assertEquals(10, item.sellIn);
+        assertEquals(20, item.quality);   
     }
 
 }
