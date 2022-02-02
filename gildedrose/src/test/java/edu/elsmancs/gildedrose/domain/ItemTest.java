8ccc32384a11f4458169be75d707aa5906b63f9f
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-02 22:50:28 +0100

    refactor: extend from StockItem

--- gildedrose/src/test/java/edu/elsmancs/gildedrose/domain/ItemTest.java
+++ gildedrose/src/test/java/edu/elsmancs/gildedrose/domain/ItemTest.java
@@ -2,13 +2,18 @@ package edu.elsmancs.gildedrose.domain;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class ItemTest {
-    
+public class ItemTest extends StockItem{
+
+    ItemTest(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+        //TODO Auto-generated constructor stub
+    }
+
     @Test
     public void createItem() {
 
-        Item item = new Item("+5 Dexterity Vest", 10, 20);
-        assertEquals("+5 Dexterity Vest", item.getName());
+        StockItem item = new StockItem("+5 Dexterity Vest", 10, 20);
+        assertEquals("+5 Dexterity Vest", item.name);
         assertEquals(10, item.getSellIn(), 0);
         assertEquals(20, item.getQuality(), 0);
         System.out.println(item);    
