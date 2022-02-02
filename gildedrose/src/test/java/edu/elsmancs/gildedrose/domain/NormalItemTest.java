4258b5bd6e609446dc23d0d7a93228b35a2aede7
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-02 23:43:11 +0100

    test(normalItemDecreaseSellIn): add test to saw if sell in decrease when 4 days are passed

--- gildedrose/src/test/java/edu/elsmancs/gildedrose/domain/NormalItemTest.java
+++ gildedrose/src/test/java/edu/elsmancs/gildedrose/domain/NormalItemTest.java
@@ -7,23 +7,32 @@ public class NormalItemTest{
     @Test
     public void createNormalItem() {
         NormalItem item = new NormalItem("Elixir of the Mongoose", 5, 7);
-        assertEquals(7, item.getQuality(), 7);
-        assertEquals(5, item.getSellIn(), 5);
-        assertEquals("Elixir of the Mongoose", item.getName(),"Elixir of the Mongoose");
+        assertEquals(7, item.getQuality());
+        assertEquals(5, item.getSellIn());
+        assertEquals("Elixir of the Mongoose", item.getName());
     }
 
     @Test
     public void normalItemSellInZero() {
         NormalItem item = new NormalItem("Elixir of the Mongoose", 0, 7);
         item.updateQuality();
-        assertEquals(0, item.getQuality(), 7);
+        assertEquals(0, item.getQuality());
     }
 
     @Test
     public void normalItemSellInNotZero() {
         NormalItem item = new NormalItem("Elixir of the Mongoose", 1, 7);
         item.updateQuality();
-        assertEquals(6, item.getQuality(), 7);
+        assertEquals(6, item.getQuality());
+    }
+
+    @Test
+    public void normalItemDecreaseSellIn() {
+        NormalItem item = new NormalItem("Elixir of the Mongoose", 5, 7);
+        
+        for (int i = 0; i < 4; i++){item.updateQuality();}
+        
+        assertEquals(1, item.getSellIn());
     }
 
     
