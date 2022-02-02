22167031a7b2527a6b1cf1d87f697b425b949514
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-03 00:29:10 +0100

    feat(SulfurasItem): add Sulfuras item class

---
+++ gildedrose/src/main/java/edu/elsmancs/gildedrose/domain/SulfurasItem.java
@@ -0,0 +1,13 @@
+package edu.elsmancs.gildedrose.domain;
+
+public class SulfurasItem extends NormalItem {
+
+    public SulfurasItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+        //TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    public void updateQuality() {}
+
+}
