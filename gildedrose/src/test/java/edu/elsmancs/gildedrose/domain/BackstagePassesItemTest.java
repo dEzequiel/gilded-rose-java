7a1355b5dd4374f61744ac01c5a16904f2ac27a1
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-03 00:45:13 +0100

    test(sellInEqualLessTenDaysAndGreaterFiveDays): add test if sell in is placed in 9,8,7,6

--- gildedrose/src/test/java/edu/elsmancs/gildedrose/domain/BackstagePassesItemTest.java
+++ gildedrose/src/test/java/edu/elsmancs/gildedrose/domain/BackstagePassesItemTest.java
@@ -12,4 +12,11 @@ public class BackstagePassesItemTest {
         assertEquals("Backstage passes to a TAFKAL80ETC concert", item.getName());
     }
 
+    @Test
+    public void sellInEqualLessTenDaysAndGreaterFiveDays() {
+        BackstagePassesItem item = new BackstagePassesItem("Backstage passes to a TAFKAL80ETC concert", 10, 20);
+        item.updateQuality()
+        assertEquals(22, item.getQuality());
+        assertEquals(9, item.getSellIn());
+    }
 }
