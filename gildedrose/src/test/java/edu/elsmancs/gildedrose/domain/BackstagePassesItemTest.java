2d184c20608bc8c5d2c2180209f6da6327c795c4
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-03 00:44:07 +0100

    test(createBackstagePasses): add test for Backstage passes item creation

---
+++ gildedrose/src/test/java/edu/elsmancs/gildedrose/domain/BackstagePassesItemTest.java
@@ -0,0 +1,15 @@
+package edu.elsmancs.gildedrose.domain;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BackstagePassesItemTest {
+    
+    @Test
+    public void createBackstagePasses() {
+        BackstagePassesItem item = new BackstagePassesItem("Backstage passes to a TAFKAL80ETC concert", 10, 20);
+        assertEquals(20, item.getQuality());
+        assertEquals(10, item.getSellIn());
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", item.getName());
+    }
+
+}
